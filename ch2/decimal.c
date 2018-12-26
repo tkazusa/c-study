3758fd2831ca595868bf88f78bf90457bf510dd6
root <takekazusa@gmail.com>
2018-12-26 16:36:05 +0000

    add decimal.c

---
+++ ch2/decimal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+
+  int a = 15, b = 0x11;
+
+  printf("%d in decimal is same as %x in hexa decimal\n", a, a);
+  printf("%x in hexa-decimal is same as %d in deximal\n", b, b);
+
+  return 0;
+
+}
