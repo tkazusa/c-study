d77ed6196f79b3fb84acc9b8610bf987454c19fa
root <takekazusa@gmail.com>
2019-01-01 07:29:44 +0000

    add chapter6

---
+++ ch6/convert_text.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  FILE *fpr, *fpw;
+  char bufr[256], bufw[256];
+  char str1[] = "dog";
+  char str2[] = "rabbit";
+  char *p, *q;
+
+  if(!(fpr = fopen("dog.txt", "r"))) {
+    printf("Failed to open a file");
+    return 1;
+  }
+  if(!(fpw = fopen("rabbit.txt", "w"))) {
+    printf("Failed to open a file");
+    return 1;
+  }
+  while(1) {
+    fgets(bufr, 256, fpr);
+    strcpy(bufw, bufr);
+    p = strstr(bufr, str1);
+    if(p) {
+      q = bufw + (p - bufr);
+      strcpy(q, str2);
+      strcpy(q+strlen(str2), p+strlen(str1));
+    }
+    fprintf(fpw, "%s", bufw);
+    if(feof(fpr))
+      break;
+  }
+  fclose(fpr);
+  fclose(fpw);
+  return 0;
+}
